c1d4fd1b7fd015a352df7446c267f5ee7d901913
taz <tsk.nakamura@gmail.com>
2022-03-20 12:29:48 +0900

    uniswap v3 twap

--- contracts/UniswapV3Twap.sol
+++ contracts/UniswapV3Twap.sol
@@ -1,13 +1,59 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity 0.7.6;
 
+import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol";
+import "@uniswap/v3-periphery/contracts/libraries/OracleLibrary.sol";
+
 /*
+TODO:
 tick
-price = 1.001 ^ tick
+- price = 1.001 ^ tick
+- how to calculate price from tick
 tick cumulative
+consult
+getQuoteAtTick
 increaseObservationCardinalityNext()
 */
 
-
 contract UniswapV3Twap {
+    address public immutable token0;
+    address public immutable token1;
+    address public immutable pool;
+
+    constructor(
+        address _factory,
+        address _token0,
+        address _token1,
+        uint24 _fee
+    ) {
+        token0 = _token0;
+        token1 = _token1;
+
+        address _pool = IUniswapV3Factory(_factory).getPool(
+            _token0,
+            _token1,
+            _fee
+        );
+        require(_pool != address(0), "pool doesn't exist");
+
+        pool = _pool;
+    }
+
+    function estimateAmountOut(
+        address tokenIn,
+        uint128 amountIn,
+        uint32 secondsAgo
+    ) external view returns (uint amountOut) {
+        require(tokenIn == token0 || tokenIn == token1, "invalid token");
+
+        address tokenOut = tokenIn == token0 ? token1 : token0;
+
+        (int24 tick, ) = OracleLibrary.consult(pool, secondsAgo);
+        amountOut = OracleLibrary.getQuoteAtTick(
+            tick,
+            amountIn,
+            tokenIn,
+            tokenOut
+        );
+    }
 }
