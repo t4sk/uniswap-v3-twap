72e602b6eecc9a9c9bf361f06125252820f35877
t4sk <tsk.nakamura@gmail.com>
2022-04-13 19:59:51 +0900

    UniswapV3Twap - copy code from Oracle library

--- contracts/UniswapV3Twap.sol
+++ contracts/UniswapV3Twap.sol
@@ -52,7 +52,30 @@ contract UniswapV3Twap {
 
         address tokenOut = tokenIn == token0 ? token1 : token0;
 
-        (int24 tick, ) = OracleLibrary.consult(pool, secondsAgo);
+        // (int24 tick, ) = OracleLibrary.consult(pool, secondsAgo);
+
+        // Code copied from OracleLibrary.sol, consult()
+        uint32[] memory secondsAgos = new uint32[](2);
+        secondsAgos[0] = secondsAgo;
+        secondsAgos[1] = 0;
+
+        // int56 since tick * time = int24 * uint32
+        // 56 = 24 + 32
+        (int56[] memory tickCumulatives, ) = IUniswapV3Pool(pool).observe(
+            secondsAgos
+        );
+
+        int56 tickCumulativesDelta = tickCumulatives[1] - tickCumulatives[0];
+
+        // int56 / uint32 = int24
+        int24 tick = int24(tickCumulativesDelta / secondsAgo);
+        // Always round to negative infinity
+        if (
+            tickCumulativesDelta < 0 && (tickCumulativesDelta % secondsAgo != 0)
+        ) {
+            tick--;
+        }
+
         amountOut = OracleLibrary.getQuoteAtTick(
             tick,
             amountIn,
